3976ca82abefc402e5ddf1a01280235f7325bbe9
Flavio Curella <flavio.curella@gmail.com>
2015-12-23 17:59:09 -0600

    mark as production/stable

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(exclude=['*.tests', 'lextab.py', 'parsetab.py']),
     platforms=["any"],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
